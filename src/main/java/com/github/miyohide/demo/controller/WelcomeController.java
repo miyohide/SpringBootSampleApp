567ad82df81781678215121d811e394357e98d6d
Hidenori Miyoshi <miyohide@gmail.com>
2019-11-08 20:25:56 +0900

    create welcome page and controller.

---
+++ src/main/java/com/github/miyohide/demo/controller/WelcomeController.java
@@ -0,0 +1,12 @@
+package com.github.miyohide.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WelcomeController {
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+}
