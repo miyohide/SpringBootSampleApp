211d9c636ca52b62dceb3016c3212f8f3dd49bd4
Hidenori Miyoshi <miyohide@gmail.com>
2019-11-08 18:57:02 +0900

    create Domain class.

---
+++ src/main/java/com/github/miyohide/demo/domain/Todo.java
@@ -0,0 +1,45 @@
+package com.github.miyohide.demo.domain;
+
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDateTime;
+
+public class Todo {
+    @Id
+    private String todoId;
+    private String todoTitle;
+    private boolean finished;
+    private LocalDateTime createdAt;
+
+    public String getTodoId() {
+        return todoId;
+    }
+
+    public void setTodoId(String todoId) {
+        this.todoId = todoId;
+    }
+
+    public String getTodoTitle() {
+        return todoTitle;
+    }
+
+    public void setTodoTitle(String todoTitle) {
+        this.todoTitle = todoTitle;
+    }
+
+    public boolean isFinished() {
+        return finished;
+    }
+
+    public void setFinished(boolean finished) {
+        this.finished = finished;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
